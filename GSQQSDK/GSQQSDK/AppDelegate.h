ded35905ed43a57b9bc3b2f4f3be4f3f3cc47276
GarryLance <Garry@zhiMingdeMac-mini.local>
2016-08-18 11:59:01 +0800

    3.1.0

---
+++ GSQQSDK/GSQQSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSQQSDK
+//
+//  Created by Garry on 16/8/18.
+//  Copyright © 2016年 Garry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
